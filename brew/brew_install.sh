a2da48f2180fbebcefa5e17509acca88624abe35
Tarek Allam <t.allam.jr@gmail.com>
2019-03-06 21:21:34 +0000

    Moving installation of brew packages to separate file that can be called from within the main install.sh script

---
+++ brew/brew_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Brew install all pacakges listed in brewlist and brewcasklist
+# brew info `grep $(find . -type f -name ".brewlist")`
+# brew info `grep $(find . -type f -name ".brewlist")`
+brew info `cat $(find . -type f -name ".brewlist")`
+
+operatingSystem=`uname`
+if [ $operatingSystem == "Darwin" ]; then
+    # Brew install all applications listed in brewcasklist
+    brew cask info `cat $(find . -type f -name ".brewcasklist")`
+    # brew cask info `cat ".brewcasklist`
+    conda_prefix="/usr/local/anaconda3"
+    export PATH="$HOME/anaconda3/bin:$PATH"
+elif [[ $operatingSystem == Linux ]]; then
+    wget https://repo.continuum.io/archive/Anaconda3-5.3.0-Linux-x86_64.sh -O ~/anaconda.sh && bash ~/anaconda.sh -b -p ~/anaconda3 && rm ~/anaconda.sh
+    conda_prefix="$HOME/anaconda3"
+fi
+source $HOME/.bashrc 2> /dev/null
