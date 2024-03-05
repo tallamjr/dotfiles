4d84914d33bfb073f5b4bdfc1151f243f5705b33
Tarek Allam Jr <t.allam.jr@gmail.com>
2024-03-05 13:20:47 +0000

    wip: Add configs for using neovim, lazy.nvim, LazyVim
    	new file:   config/.config/github-copilot/versions.json
    	modified:   config/.config/nvim/lazy-lock.json
    	modified:   config/.config/nvim/lazyvim.json
    	modified:   config/.config/nvim/lua/config/keymaps.lua
    	modified:   config/.config/nvim/lua/config/lazy.lua
    	modified:   config/.config/nvim/lua/config/options.lua
    	new file:   config/.config/nvim/lua/plugins/obsidian.lua
    	new file:   config/.config/nvim/lua/plugins/telescope.lua
    	new file:   config/.config/nvim/lua/plugins/treesitter.lua

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -3,5 +3,13 @@
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "kj", "<esc>", { desc = "Lazy" })
-vim.keymap.set("n", "<leader><space>", "<cmd>w<cr>")
+vim.keymap.set("i", "kj", "<esc>", { desc = "Lazy" }) -- new esacpe
+vim.keymap.set("n", "<leader><space>", "<cmd>w<cr>") -- save
+vim.keymap.set("n", "<leader>qq", "<cmd>q!<cr>") -- quick quit
+-- toggle highlight search
+vim.keymap.set(
+  "n",
+  "<leader>/",
+  [[ (&hls && v:hlsearch ? ':nohls' : ':set hls')."\n" <BAR> redraw<CR>]],
+  { silent = true, expr = true, desc = "toggles highlighting" }
+)
