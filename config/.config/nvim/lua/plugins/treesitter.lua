fb25e6632fb22868c6d4ea93e8f8865d63c69454
Tarek Allam Jr <t.allam.jr@gmail.com>
2024-09-09 23:40:06 +0100

    Update with astrovim defaults

--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,15 @@
-return
--- add more treesitter parsers
-{
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
+return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
-      "bash",
-      "html",
-      "javascript",
-      "json",
       "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "regex",
-      "tsx",
-      "typescript",
       "vim",
-      "yaml",
+      -- add more arguments for adding more treesitter parsers
     },
   },
-},
-
--- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
--- would overwrite `ensure_installed` with the new value.
--- If you'd rather extend the default config, use the code below instead:
-{
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add tsx and treesitter
-    vim.list_extend(opts.ensure_installed, {
-      "tsx",
-      "typescript",
-    })
-  end,
 }
