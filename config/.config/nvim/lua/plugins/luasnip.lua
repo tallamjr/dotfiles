9053f47bf38eca298a83fecc6fa9fd8c172f9369
Tarek Allam Jr <t.allam.jr@gmail.com>
2024-09-19 11:39:00 +0100

    refactor(nvim): move code into standalone files

--- config/.config/nvim/lua/plugins/luasnip.lua
+++ config/.config/nvim/lua/plugins/luasnip.lua
@@ -3,6 +3,8 @@ return {
   config = function(plugin, opts)
     -- include the default astronvim config that calls the setup call
     require "astronvim.plugins.configs.luasnip"(plugin, opts)
+    local luasnip = require "luasnip"
+    luasnip.filetype_extend("javascript", { "javascriptreact" })
     -- load snippets paths
     -- require("luasnip.loaders.from_vscode").lazy_load {
     require("luasnip.loaders.from_snipmate").load {
