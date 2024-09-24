9de74f3d5d583a2d981bbe1beb8c74ce02b809cb
Tarek Allam Jr <t.allam.jr@gmail.com>
2024-09-24 13:15:00 +0100

    fix(nvim): remove pyright and pylint. ruff only

--- config/.config/nvim/lua/plugins/linting.lua
+++ config/.config/nvim/lua/plugins/linting.lua
@@ -9,8 +9,8 @@ return {
       typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" },
-      python = { "pylint" },
+      python = { "ruff" },
+      git = { "gitlint" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
--- config/.config/nvim/lua/plugins/mason.lua
+++ config/.config/nvim/lua/plugins/mason.lua
@@ -44,8 +44,7 @@ return {
           "stylua", -- lua formatter
           "isort", -- python formatter
           "black", -- python formatter
-          "pylint", -- python linter
-          "eslint_d", -- js linter
+          "ruff", -- python linter/formatter
         },
       }
     end,
