f3afa70228ce7fce4046082544b62e0d11d884c0
Tarek Allam Jr <t.allam.jr@gmail.com>
2024-09-24 13:06:51 +0100

    feat(git): automatic tracking of forked branches
    Saves on manual:
    
      $ git checkout --track origin/<branch-name>

--- config/.config/nvim/lua/plugins/copilot.lua
+++ config/.config/nvim/lua/plugins/copilot.lua
@@ -1,5 +1,28 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 return {
   "github/copilot.vim",
+
+  config = function()
+    -- Disable default Copilot tab mapping
+    vim.g.copilot_no_tab_map = true
+
+    -- Remap <Tab> to accept Copilot suggestions only
+    vim.api.nvim_set_keymap("i", "<Tab>", "copilot#Accept('<Tab>')", { silent = true, expr = true })
+    vim.api.nvim_set_keymap("i", "<S-Tab>", "<C-v><Tab>", { noremap = true, silent = true }) -- Prevent Shift-Tab from inserting tabs
+
+    -- Optional: Configure nvim-cmp (or other autocompletion plugin) to not use Tab
+    local cmp = require "cmp"
+    cmp.setup {
+      mapping = {
+        -- Remove Tab mappings for navigating the completion menu
+        ["<Tab>"] = function(fallback)
+          fallback() -- Do nothing, fallback to default behavior (e.g., indenting)
+        end,
+        ["<S-Tab>"] = function(fallback)
+          fallback() -- Do nothing, fallback to default behavior
+        end,
+      },
+    }
+  end,
 }
