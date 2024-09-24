16bd61a79105f218e13121319ea63b227fc3b76f
Tarek Allam Jr <t.allam.jr@gmail.com>
2024-09-24 13:17:13 +0100

    refactor(nvim): wip: custom ui for diagnostics with "trouble"

--- config/.config/nvim/lua/plugins/trouble.lua
+++ config/.config/nvim/lua/plugins/trouble.lua
@@ -22,4 +22,36 @@ return {
     { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
     { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
   },
+  -- config = function()
+  --   -- Setup for trouble.nvim
+  --   require("trouble").setup {
+  --     position = "bottom", -- Position of the trouble window (can be top, bottom, left, right)
+  --     height = 10, -- Height of the trouble window (for top/bottom)
+  --     width = 50, -- Width of the trouble window (for left/right)
+  --     icons = true, -- Enable icons (requires nvim-web-devicons)
+  --     mode = "workspace_diagnostics", -- Default mode (can be "workspace_diagnostics", "document_diagnostics", etc.)
+  --     auto_open = false, -- Automatically open when diagnostics are found
+  --     auto_close = true, -- Automatically close when no diagnostics are left
+  --     use_diagnostic_signs = true, -- Use the LSP diagnostic signs
+  --   }
+  --
+  --   -- Enable inline diagnostics (virtual text)
+  --   vim.diagnostic.config {
+  --     virtual_text = {
+  --       -- prefix = "●", -- Symbol to use for virtual text (change this if you'd like)
+  --       spacing = 4, -- Spacing between the code and the virtual text
+  --     },
+  --     signs = true, -- Show signs in the sign column
+  --     underline = true, -- Underline the text that has diagnostics
+  --     update_in_insert = false, -- Don't update diagnostics in insert mode
+  --     severity_sort = true, -- Sort diagnostics by severity
+  --   }
+  --
+  --   -- Custom symbols for diagnostics (if you want different symbols for LSP signs)
+  --   local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  --   for type, icon in pairs(signs) do
+  --     local hl = "DiagnosticSign" .. type
+  --     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  --   end
+  -- end,
 }
