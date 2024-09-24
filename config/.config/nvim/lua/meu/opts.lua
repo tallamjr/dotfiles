e135d8fd2f29cfed9404799e0d403c61e5e8b57f
Tarek Allam Jr <t.allam.jr@gmail.com>
2024-09-24 13:09:57 +0100

    refactor(nvim): be verbose when writing opts

--- config/.config/nvim/lua/meu/opts.lua
+++ config/.config/nvim/lua/meu/opts.lua
@@ -1,63 +1,85 @@
-local opt = vim.opt -- for conciseness
-
 -- line numbers
-opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+vim.opt.relativenumber = true -- show relative line numbers
+vim.opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
+vim.opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+vim.opt.shiftwidth = 2 -- 2 spaces for indent width
+vim.opt.expandtab = true -- expand tab to spaces
+vim.opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
-opt.wrap = false -- disable line wrapping
+vim.opt.wrap = false -- disable line wrapping
 
 -- search settings
-opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+vim.opt.ignorecase = true -- ignore case when searching
+vim.opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
-opt.cursorline = true -- highlight the current cursor line
+vim.opt.cursorline = true -- highlight the current cursor line
 
 -- appearance
 
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
-opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+vim.opt.termguicolors = true
+vim.opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+vim.opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+vim.opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
-opt.clipboard:append "unnamedplus" -- use system clipboard as default register
+vim.opt.clipboard:append "unnamedplus" -- use system clipboard as default register
 
 -- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+vim.opt.splitright = true -- split vertical window to the right
+vim.opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
-opt.swapfile = false
+vim.opt.swapfile = false
 
-opt.cmdheight = 1
+vim.opt.cmdheight = 1
 
 -- set line length for all filetypes
-opt.textwidth = 80
+vim.opt.textwidth = 80
 -- autocmds
--- vim.api.nvim_create_autocmd("BufWinEnter", {
---   pattern = { "*.md" },
---   callback = function()
---     vim.opt.colorcolumn = "80"
---     vim.opt.textwidth = 80
---   end,
--- })
---
--- vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
---   pattern = { "*.md" },
---   callback = function()
---     vim.opt.colorcolumn = "120"
---     vim.opt.textwidth = 120
---   end,
--- })
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = { "*.md" },
+  callback = function()
+    vim.opt.colorcolumn = "80"
+    vim.opt.textwidth = 80
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+  pattern = { "*.md" },
+  callback = function()
+    vim.opt.colorcolumn = "120"
+    vim.opt.textwidth = 120
+  end,
+})
+
+-- from theprimeagen/set.lua
+vim.opt.guicursor = ""
+
+vim.opt.smartindent = true
+
+vim.opt.backup = false
+vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 10
+vim.opt.isfname:append "@-@"
+
+vim.opt.updatetime = 50
+--------------------------------------
+
+-- Enable spell checking
+vim.opt.spell = true
+
+-- Set spell languages to UK English, Spanish, and Portuguese
+-- vim.opt.spelllang = { "en_gb", "es", "pt" }
