a8899e41daa02d572cd5b2a0f6da931faf5a5790
Tarek Allam Jr <t.allam.jr@gmail.com>
2024-09-28 14:34:56 +0100

    feat(nvim): add markdown functionality for toc

--- config/.config/nvim/lua/plugins/user.lua
+++ config/.config/nvim/lua/plugins/user.lua
@@ -44,4 +44,12 @@ return {
 
   -- Disable pop-up notifications
   { "rcarriga/nvim-notify", enabled = false },
+  {
+    "hedyhli/markdown-toc.nvim",
+    ft = "markdown", -- Lazy load on markdown filetype
+    cmd = { "Mtoc" }, -- Or, lazy load on "Mtoc" command
+    opts = {
+      -- Your configuration here (optional)
+    },
+  },
 }
