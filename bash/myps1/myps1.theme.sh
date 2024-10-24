c3e9014899713f3bf23d4ca4a2737ea364d2f993
Tarek Allam Jr <t.allam.jr@gmail.com>
2024-10-24 16:14:59 +0100

    Add custom ps1 for oh my bash

---
+++ bash/myps1/myps1.theme.sh
@@ -0,0 +1,8 @@
+
+function _omb_theme_PROMPT_COMMAND() {
+  PS1='\[\e[01;30m\]\t`if [ $? = 0 ]; then echo "\[\e[32m\] ✔ "; else echo "\[\e[31m\] ✘ "; fi`\[\e[01;34m\]\w\[\e[00m\] `[[ $(git status 2> /dev/null | head -n5 | tail -n1) == "nothing to commit, working tree clean" ]] && echo "\[\e[01;32m\]"$(__git_ps1 "(%s)") || echo "\[\e[01;31m\]"$(__git_ps1 "(%s)")` \[\e[00m\]:: '
+  PROMPT_DIRTRIM=4
+}
+
+_omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
+
