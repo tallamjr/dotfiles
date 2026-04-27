6aaf34b2ca7c81a010d60c7b2a3d145e04eaac0a
Tarek Allam Jr <t.allam.jr@gmail.com>
2026-04-27 12:27:45 +0100

    chore(rust): install kicad-ctlrs via cargo install --git

--- rust/install-rust-with-crates.sh
+++ rust/install-rust-with-crates.sh
@@ -4,6 +4,8 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Install crates
 xargs -0 -n 1 cargo install < <(tr \\n \\0 <crates)
+# Install kicad-ctlrs from GitHub (not on crates.io)
+cargo install --git https://github.com/tallamjr/kicad-ctlrs --locked
 # rustup installs
 rustup update
 rustup component add \
