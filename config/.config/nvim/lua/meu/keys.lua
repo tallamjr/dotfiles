e5be22d6e0070002ae8805f6c151adde36a75f66
Tarek Allam Jr <t.allam.jr@gmail.com>
2026-05-14 15:09:42 +0100

    chore(nvim): remove orphaned Go err-handling snippet from <leader>ee
    The mapping inserted Go-style `if err != nil { return err }` boilerplate
    on <leader>ee in any filetype, and was already shadowed at runtime by
    the nvim-tree toggle bound to the same key. Removing it eliminates the
    duplicate registration and the unintended snippet in non-Go buffers.

--- config/.config/nvim/lua/meu/keys.lua
+++ config/.config/nvim/lua/meu/keys.lua
@@ -127,6 +127,4 @@ keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 keymap.set("n", "<leader>+x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
 -------------------------------------------------------------------------------
